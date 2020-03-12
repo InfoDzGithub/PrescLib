901ca357aa5b4b39b0667f53d82f2a3b3252b161
LatifaInfoDz <bouchekiflatifa1@gmail.com>
2020-03-12 21:18:47 +0100

    header and menu and footer template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { GlobalComponent } from './global/global.component';
+import { MenuComponent } from './menu/menu.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'global', component: GlobalComponent },
   {
     path: '',
     redirectTo: '/login',
@@ -19,7 +24,11 @@ const appRoutes: Routes = [
     AppComponent,
     LoginComponent,
     LogoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    GlobalComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(appRoutes)
